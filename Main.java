06fe9303f15d5b2802f9e74425642124de21fd4e
bilal-qadir123 <bilalqadir2020@gmail.com>
2024-12-19 16:04:43 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -16,19 +16,17 @@ public class Main {
         public void run() {
             synchronized (Lock1) {
                 System.out.println("Thread 1: Holding lock 1...");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    System.out.println("Thread 1: Interrupted while waiting for lock 2...");
+                try { 
+                	Thread.sleep(10); 
                 }
+                catch (InterruptedException e) {} 
                 System.out.println("Thread 1: Waiting for lock 2...");
                 synchronized (Lock2) {
                     System.out.println("Thread 1: Holding lock 1 & 2...");
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        System.out.println("Thread 1: Interrupted while waiting for lock 3...");
+                    try { 
+                    	Thread.sleep(10); 
                     }
+                    catch (InterruptedException e) {}
                     System.out.println("Thread 1: Waiting for lock 3...");
                     synchronized (Lock3) {
                         System.out.println("Thread 1: Holding lock 1, 2 & 3...");
@@ -40,21 +38,19 @@ public class Main {
 
     private static class Thread2 extends Thread {
         public void run() {
-            synchronized (Lock3) {
-                System.out.println("Thread 2: Holding lock 3...");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    System.out.println("Thread 2: Interrupted while waiting for lock 1...");
+            synchronized (Lock2) {
+                System.out.println("Thread 2: Holding lock 2...");
+                try { 
+                	Thread.sleep(10); 
                 }
-                System.out.println("Thread 2: Waiting for lock 1...");
-                synchronized (Lock2) {
+                catch (InterruptedException e) {}
+                System.out.println("Thread 2: Waiting for lock 3...");
+                synchronized (Lock3) {
                     System.out.println("Thread 2: Holding lock 2 & 3...");
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        System.out.println("Thread 2: Interrupted while waiting for lock 1...");
+                    try { 
+                    	Thread.sleep(10); 
                     }
+                    catch (InterruptedException e) {}
                     System.out.println("Thread 2: Waiting for lock 1...");
                     synchronized (Lock1) {
                         System.out.println("Thread 2: Holding lock 1, 2 & 3...");
@@ -66,23 +62,21 @@ public class Main {
 
     private static class Thread3 extends Thread {
         public void run() {
-            synchronized (Lock2) {
-                System.out.println("Thread 3: Holding lock 2...");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    System.out.println("Thread 3: Interrupted while waiting for lock 3...");
+            synchronized (Lock3) {
+                System.out.println("Thread 3: Holding lock 3...");
+                try { 
+                	Thread.sleep(10); 
                 }
-                System.out.println("Thread 3: Waiting for lock 3...");
-                synchronized (Lock3) {
-                    System.out.println("Thread 3: Holding lock 2 & 3...");
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        System.out.println("Thread 3: Interrupted while waiting for lock 1...");
+                catch (InterruptedException e) {}
+                System.out.println("Thread 3: Waiting for lock 1...");
+                synchronized (Lock1) {
+                    System.out.println("Thread 3: Holding lock 1 & 3...");
+                    try { 
+                    	Thread.sleep(10); 
                     }
-                    System.out.println("Thread 3: Waiting for lock 1...");
-                    synchronized (Lock1) {
+                    catch (InterruptedException e) {}
+                    System.out.println("Thread 3: Waiting for lock 2...");
+                    synchronized (Lock2) {
                         System.out.println("Thread 3: Holding lock 1, 2 & 3...");
                     }
                 }
