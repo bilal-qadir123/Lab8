a1c71ab19afab6e83bd0a4b1db827431f3be2808
bilal-qadir123 <bilalqadir2020@gmail.com>
2024-12-19 16:18:17 +0500

    Add files via upload

--- MainNoDeadlock.java
+++ MainNoDeadlock.java
@@ -1,4 +1,4 @@
-public class Main {
+public class MainNoDeadlock {
     public static Object Lock1 = new Object();
     public static Object Lock2 = new Object();
     public static Object Lock3 = new Object();
@@ -14,14 +14,14 @@ public class Main {
 
     private static class Thread1 extends Thread {
         public void run() {
-            synchronized (Lock2) {
+            synchronized (Lock1) {
                 System.out.println("Thread 1: Holding lock 1...");
                 try { 
                 	Thread.sleep(10); 
                 }
                 catch (InterruptedException e) {} 
                 System.out.println("Thread 1: Waiting for lock 2...");
-                synchronized (Lock1) {
+                synchronized (Lock2) {
                     System.out.println("Thread 1: Holding lock 1 & 2...");
                     try { 
                     	Thread.sleep(10); 
@@ -38,14 +38,14 @@ public class Main {
 
     private static class Thread2 extends Thread {
         public void run() {
-            synchronized (Lock2) {
+            synchronized (Lock1) {
                 System.out.println("Thread 2: Holding lock 2...");
                 try { 
                 	Thread.sleep(10); 
                 }
                 catch (InterruptedException e) {}
                 System.out.println("Thread 2: Waiting for lock 3...");
-                synchronized (Lock1) {
+                synchronized (Lock2) {
                     System.out.println("Thread 2: Holding lock 2 & 3...");
                     try { 
                     	Thread.sleep(10); 
@@ -62,14 +62,14 @@ public class Main {
 
     private static class Thread3 extends Thread {
         public void run() {
-            synchronized (Lock2) {
+            synchronized (Lock1) {
                 System.out.println("Thread 3: Holding lock 3...");
                 try { 
                 	Thread.sleep(10); 
                 }
                 catch (InterruptedException e) {}
                 System.out.println("Thread 3: Waiting for lock 1...");
-                synchronized (Lock1) {
+                synchronized (Lock2) {
                     System.out.println("Thread 3: Holding lock 1 & 3...");
                     try { 
                     	Thread.sleep(10); 
